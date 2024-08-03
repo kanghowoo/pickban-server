ebec1f60aff3e32328079c71b57e918d8afcc636
kanghowoo <kanghowoo@github.com>
2024-08-03 23:59:17 +0900

    add: init project

---
+++ src/test/java/com/pickban/PickbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
