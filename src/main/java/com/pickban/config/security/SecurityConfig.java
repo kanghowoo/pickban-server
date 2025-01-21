b2240f6c1bc694af75ad8723f2ca47d024c52d60
kanghowoo <tiger452003@gmail.com>
2025-01-22 01:25:38 +0900

    chore: security CORS configuration

--- src/main/java/com/pickban/config/security/SecurityConfig.java
+++ src/main/java/com/pickban/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -84,6 +85,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults());
         http
                 .csrf(CsrfConfigurer::disable);
         http
