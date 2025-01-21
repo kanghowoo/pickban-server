252ee5716eb9bbc2ee3e44469e1ed063adad770b
kanghowoo <tiger452003@gmail.com>
2025-01-22 03:51:15 +0900

    fix: read e-mail template file using InputStream

--- src/main/java/com/pickban/service/MailTemplateService.java
+++ src/main/java/com/pickban/service/MailTemplateService.java
@@ -1,6 +1,7 @@
 package com.pickban.service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,22 +9,23 @@ import java.nio.file.Path;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 
 @Service
 public class MailTemplateService {
 
     public String getVerifyEmailTemplate() throws IOException {
         Resource resource = new ClassPathResource("templates/verification-mail.html");
-        Path path = resource.getFile().toPath();
-
-        return Files.readString(path, StandardCharsets.UTF_8);
+        try (InputStream inputStream = resource.getInputStream()) {
+            return StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        }
     }
 
     public String getPasswordResetEmailTemplate() throws IOException {
         Resource resource = new ClassPathResource("templates/password-reset-mail.html");
-        Path path = resource.getFile().toPath();
-
-        return Files.readString(path, StandardCharsets.UTF_8);
+        try (InputStream inputStream = resource.getInputStream()) {
+            return StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        }
     }
 
 }
